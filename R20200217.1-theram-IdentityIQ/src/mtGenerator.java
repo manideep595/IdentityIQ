f833bacfee08db6730a9a0bc1e8b700fb60c2d93
manideeptheratpali <manideeptheratpali@192.168.0.13>
2020-02-21 23:35:02 -0600

    Initial checkin of MT-Generator java object which will be used in whole project as an unique feature.

---
+++ R20200217.1-theram-IdentityIQ/src/mtGenerator.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+
+/**
+ * @author manideeptheratpali
+ *
+ */
+abstract class mtGenerator {
+
+	/**
+	 * 
+	 */
+	public mtGenerator() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
